7cfbd373d7bcc65a81e0e48dcec41110308c450f
ACampos0297 <acampos0297@tamu.edu>
2019-09-10 21:50:42 -0500

    Reorganized code

--- CSCE-463-HW1/CSCE-463-HW1.cpp
+++ CSCE-463-HW1/CSCE-463-HW1.cpp
@@ -213,227 +213,295 @@ int main(int argc, char** argv)
 					printf("failed with invalid scheme\n");
 				}
 				
-				if (host != "")
+if (host != "")
+{
+	//check host uniqueness
+	printf("\tChecking host uniqueness... ");
+	int prevSize = seenHosts.size();
+	seenHosts.insert(host);
+	if (seenHosts.size() > prevSize)
+	{
+		//unique host
+		cout << "passed" << endl;
+		cout << "\tDoing DNS... ";
+		//start clock 
+		clock_t t = clock();
+
+		// structure used in DNS lookups
+		struct hostent* remote;
+		// structure for connecting to server
+		struct sockaddr_in server;
+		// first assume that the string is an IP address
+		DWORD IP = inet_addr(host.c_str());
+
+		int prevIPSize = seenIPs.size();
+
+		if (IP == INADDR_NONE)
+		{
+			// if not a valid IP, then do a DNS lookup
+			if ((remote = gethostbyname(host.c_str())) == NULL)
+			{
+				printf("Invalid string: neither FQDN, nor IP address");
+			}
+			else // take the first IP address and copy into sin_addr
+			{
+				memcpy((char*) & (server.sin_addr), remote->h_addr, remote->h_length);
+				//found IP from DNS lookup, output time and IP
+				printf("done in %.0f ms, found %s", (double)(clock() - t), inet_ntoa(server.sin_addr));
+				string ip = inet_ntoa(server.sin_addr);
+				seenIPs.insert(inet_addr(ip.c_str()));
+			}
+		}
+		else
+		{
+			printf("done in 0 ms, found %s", host.c_str());
+			// if a valid IP, directly drop its binary version into sin_addr
+			server.sin_addr.S_un.S_addr = IP;
+			seenIPs.insert(IP);
+		}
+
+		cout << "\n\tChecking IP Uniqueness...";
+		if (seenIPs.size() > prevIPSize)
+		{
+			cout << " passed\n";
+			//unique 
+			//Connect on robots
+			cout << "\tConnecting on robots...";
+							
+							
+							
+			// open a TCP socket
+			SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+			if (sock == INVALID_SOCKET)
+			{
+				printf("socket() generated error %d\n", WSAGetLastError());
+			}
+			else
+			{
+				// setup the port # and protocol type
+				server.sin_family = AF_INET;
+				if (port != "")
 				{
-					//check host uniqueness
-					printf("\tChecking host uniqueness... ");
-					int prevSize = seenHosts.size();
-					seenHosts.insert(host);
-					if (seenHosts.size() > prevSize)
-					{
-						//unique host
-						cout << "passed" << endl;
-						cout << "\tDoing DNS... ";
-						//start clock 
-						clock_t t = clock();
+					server.sin_port = htons((unsigned short)stoi(port));
+				}
+				else
+					server.sin_port = htons(80);		// host-to-network flips the byte order
 
-						// structure used in DNS lookups
-						struct hostent* remote;
-						// structure for connecting to server
-						struct sockaddr_in server;
-						// first assume that the string is an IP address
-						DWORD IP = inet_addr(host.c_str());
-
-						int prevIPSize = seenIPs.size();
-
-						if (IP == INADDR_NONE)
-						{
-							// if not a valid IP, then do a DNS lookup
-							if ((remote = gethostbyname(host.c_str())) == NULL)
-							{
-								printf("Invalid string: neither FQDN, nor IP address");
-							}
-							else // take the first IP address and copy into sin_addr
-							{
-								memcpy((char*) & (server.sin_addr), remote->h_addr, remote->h_length);
-								//found IP from DNS lookup, output time and IP
-								printf("done in %.0f ms, found %s", (double)(clock() - t), inet_ntoa(server.sin_addr));
-								string ip = inet_ntoa(server.sin_addr);
-								seenIPs.insert(inet_addr(ip.c_str()));
-							}
-						}
-						else
-						{
-							printf("done in 0 ms, found %s", host.c_str());
-							// if a valid IP, directly drop its binary version into sin_addr
-							server.sin_addr.S_un.S_addr = IP;
-							seenIPs.insert(IP);
-						}
-
-						cout << "\n\tChecking IP Uniqueness...";
-						if (seenIPs.size() > prevIPSize)
-						{
-							cout << " passed\n";
-							//unique 
-							//Connect on robots
-							cout << "\tConnecting on robots...";
-							
-							
-							
-							// open a TCP socket
-							SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-							if (sock == INVALID_SOCKET)
-							{
-								printf("socket() generated error %d\n", WSAGetLastError());
-							}
-							else
-							{
-								// setup the port # and protocol type
-								server.sin_family = AF_INET;
-								if (port != "")
-								{
-									server.sin_port = htons((unsigned short)stoi(port));
-								}
-								else
-									server.sin_port = htons(80);		// host-to-network flips the byte order
-
-								// connect to the server on port given
-								t = clock();
-								if (connect(sock, (struct sockaddr*) & server, sizeof(struct sockaddr_in)) == SOCKET_ERROR)
-								{
-									printf("Connection error: %d", WSAGetLastError());
-								}
-								else
-								{
-									printf("done in %.0f ms", (double)(clock() - t));
+				// connect to the server on port given
+				t = clock();
+				if (connect(sock, (struct sockaddr*) & server, sizeof(struct sockaddr_in)) == SOCKET_ERROR)
+				{
+					printf("Connection error: %d", WSAGetLastError());
+				}
+				else
+				{
+					printf("done in %.0f ms", (double)(clock() - t));
 								
-									// send HTTP requests here
-									printf("\n\tLoading... ");
+					// send HTTP requests here
+					printf("\n\tLoading... ");
 
-									char headMethod[] = "HEAD %s HTTP/1.0\r\nUser-Agent: accrawler/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n";
-									int reqBuffLen = strlen(host.c_str()) + strlen(path.c_str()) + strlen(headMethod) - 4;
-									char* sendBuf = new char[reqBuffLen + 1];
+					char headMethod[] = "HEAD %s HTTP/1.0\r\nUser-Agent: accrawler/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n";
+					int reqBuffLen = strlen(host.c_str()) + strlen(path.c_str()) + strlen(headMethod) - 4;
+					char* sendBuf = new char[reqBuffLen + 1];
 
-									sprintf(sendBuf, headMethod, path.c_str(), host.c_str());
+					sprintf(sendBuf, headMethod, path.c_str(), host.c_str());
 									
-									// place request into buf
-									if (send(sock, sendBuf, reqBuffLen, 0) == SOCKET_ERROR)
-									{
-										printf("Send error: %d\n", WSAGetLastError());
-									}
-									else
-									{
-										Socket recvSocket(sock);
-										clock_t t = clock();
-										if (recvSocket.Read(ROBOT_SIZE))
-										{
-											printf("done in %.0f ms with %u bytes", (double)(clock() - t), recvSocket.bytesReceived());
-											// close the socket to this server; open again for the next one
-											closesocket(sock);
+					// place request into buf
+					if (send(sock, sendBuf, reqBuffLen, 0) == SOCKET_ERROR)
+					{
+						printf("Send error: %d\n", WSAGetLastError());
+					}
+					else
+					{
+						Socket recvSocket(sock);
+						clock_t t = clock();
+						if (recvSocket.Read(ROBOT_SIZE))
+						{
+							printf("done in %.0f ms with %u bytes", (double)(clock() - t), recvSocket.bytesReceived());
+							// close the socket to this server; open again for the next one
+							closesocket(sock);
 
-											printf("\n\tVerifying header... ");
-											string header;
+							printf("\n\tVerifying header... ");
+							string header;
 
-											string received(recvSocket.getBuffer());
-											string status = received.substr(9, 3);
-											cout << "status code " << status;
+							string received(recvSocket.getBuffer());
+							string status = received.substr(9, 3);
+							cout << "status code " << status;
 
-											if (status[0] == '4')
-											{
-												//robot exists, download page
-												cout << "\n\t\b\b* Connecting on page...";
-												// open a TCP socket
-												SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-												if (sock == INVALID_SOCKET)
-												{
-													printf("socket() generated error %d\n", WSAGetLastError());
-												}
-												else
-												{
-													// setup the port # and protocol type
-													server.sin_family = AF_INET;
-													if (port != "")
-													{
-														server.sin_port = htons((unsigned short)stoi(port));
-													}
-													else
-														server.sin_port = htons(80);		// host-to-network flips the byte order
+	if (status[0] == '4')
+	{
+		//robot exists, download page
+		cout << "\n\t\b\b* Connecting on page...";
+		// open a TCP socket
+		SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (sock == INVALID_SOCKET)
+		{
+			printf("socket() generated error %d\n", WSAGetLastError());
+		}
+		else
+		{
+			// setup the port # and protocol type
+			server.sin_family = AF_INET;
+			if (port != "")
+			{
+				server.sin_port = htons((unsigned short)stoi(port));
+			}
+			else
+				server.sin_port = htons(80);		// host-to-network flips the byte order
 
-													// connect to the server on port given
-													t = clock();
-													if (connect(sock, (struct sockaddr*) & server, sizeof(struct sockaddr_in)) == SOCKET_ERROR)
-													{
-														printf("Connection error: %d", WSAGetLastError());
-													}
-													else
-													{
-														printf("done in %.0f ms", (double)(clock() - t));
+			// connect to the server on port given
+			t = clock();
+			if (connect(sock, (struct sockaddr*) & server, sizeof(struct sockaddr_in)) == SOCKET_ERROR)
+			{
+				printf("Connection error: %d", WSAGetLastError());
+			}
+			else
+			{
+				printf("done in %.0f ms", (double)(clock() - t));
 
-														// send HTTP requests here
-														printf("\n\tLoading... ");
+				// send HTTP requests here
+				printf("\n\tLoading... ");
 
-														char getMethod[] = "GET %s HTTP/1.0\r\nUser-Agent: accrawler/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n";
-														int reqBuffLen = strlen(host.c_str()) + strlen(path.c_str()) + strlen(getMethod) - 4;
-														char* sendBuf = new char[reqBuffLen + 1];
+				char getMethod[] = "GET %s HTTP/1.0\r\nUser-Agent: accrawler/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n";
+				int reqBuffLen = strlen(host.c_str()) + strlen(path.c_str()) + strlen(getMethod) - 4;
+				char* sendBuf = new char[reqBuffLen + 1];
 
-														sprintf(sendBuf, getMethod, path.c_str(), host.c_str());
+				sprintf(sendBuf, getMethod, path.c_str(), host.c_str());
 
-														// place request into buf
-														if (send(sock, sendBuf, reqBuffLen, 0) == SOCKET_ERROR)
-														{
-															printf("Send error: %d\n", WSAGetLastError());
-														}
-														else
-														{
-															// thread handles are stored here; they can be used to check status of threads, or kill them
-															HANDLE* handles = new HANDLE[1];
+				// place request into buf
+				if (send(sock, sendBuf, reqBuffLen, 0) == SOCKET_ERROR)
+				{
+					printf("Send error: %d\n", WSAGetLastError());
+				}
+				else
+				{
+					// thread handles are stored here; they can be used to check status of threads, or kill them
+					HANDLE* handles = new HANDLE[1];
 
-															Parameters p;
-															p.recvSocket = new Socket(sock);
-															// create a mutex for accessing critical sections (including printf); initial state = not locked
-															p.mutex = CreateMutex(NULL, 0, NULL);
-															// create a semaphore that counts the number of active threads; initial value = 0, max = 2
-															p.finished = CreateSemaphore(NULL, 0, 1, NULL);
-															// create a quit event; manual reset, initial state = not signaled
-															p.eventQuit = CreateEvent(NULL, true, false, NULL);
+					Parameters p;
+					p.recvSocket = new Socket(sock);
+					// create a mutex for accessing critical sections (including printf); initial state = not locked
+					p.mutex = CreateMutex(NULL, 0, NULL);
+					// create a semaphore that counts the number of active threads; initial value = 0, max = 2
+					p.finished = CreateSemaphore(NULL, 0, 1, NULL);
+					// create a quit event; manual reset, initial state = not signaled
+					p.eventQuit = CreateEvent(NULL, true, false, NULL);
 
-															t = clock();
-															// structure p is the shared space between the threads
-															handles[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)threadA, &p, 0, NULL);
+					t = clock();
+					// structure p is the shared space between the threads
+					handles[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)threadA, &p, 0, NULL);
 
 
-															WaitForSingleObject(handles[0], INFINITE);
-															CloseHandle(handles[0]);
+					WaitForSingleObject(handles[0], INFINITE);
+					CloseHandle(handles[0]);
 
-															// close the socket to this server; open again for the next one
-															closesocket(sock);
+					// close the socket to this server; open again for the next one
+					closesocket(sock);
 
-															printf("\n\tVerifying header... ");
-															string header;
+					printf("\n\tVerifying header... ");
+					string header;
 
-															string received(p.recvSocket->getBuffer());
-															string status = received.substr(9, 3);
-															cout << "status code " << status;
+					string received(p.recvSocket->getBuffer());
+					string status = received.substr(9, 3);
+					cout << "status code " << status;
 
-															if (status[0] == '2')
-															{
-																cout << "\n\t\b\b+ Parsing page";
-																
-															}
-														}
-													}
-												}
-											}
-										}
-									}
+					if (status[0] == '2')
+					{
+						printf("\t\b\b+ Parsing page... ");
+						received.erase(0, received.find("<html>"));
+						ofstream htmlOut("test.html");
+						if (htmlOut.is_open())
+						{
+							htmlOut << received.c_str();
+							char filename[] = "test.html";
+							// open html file
+							HANDLE hFile = CreateFile(filename, GENERIC_READ, 
+								FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING,
+								FILE_ATTRIBUTE_NORMAL, NULL);
+							// process errors
+							if (hFile == INVALID_HANDLE_VALUE)
+							{
+								printf("CreateFile failed with %d\n", GetLastError());
+								htmlOut.close();
+								remove("test.html");
+							}
+							else {
+								// get file size
+								LARGE_INTEGER li;
+								BOOL bRet = GetFileSizeEx(hFile, &li);
+								// process errors
+								if (bRet == 0)
+								{
+									printf("GetFileSizeEx error %d\n", GetLastError());
+								}
+								else {
+								// read file into a buffer
+								int fileSize = (DWORD)li.QuadPart;
+								DWORD bytesRead;
+								// allocate buffer
+								char* fileBuf = new char[fileSize];
+								// read into the buffer
+								bRet = ReadFile(hFile, fileBuf, fileSize, &bytesRead, NULL);
+								// process errors
+								if (bRet == 0 || bytesRead != fileSize)
+								{
+									printf("ReadFile failed with %d\n", GetLastError());
+								}
+								else {
+									// done with the file
+									CloseHandle(hFile);
+									// create new parser object
+									HTMLParserBase* parser = new HTMLParserBase;
+									host = "http://" + host;
+									// where this page came from; needed for construction of relative links
+									char baseUrl[MAX_URL_LEN];
+									strcpy(baseUrl, host.c_str());
+									int nLinks;
+									t = clock();
+									char* linkBuffer = parser->Parse(fileBuf, fileSize, baseUrl, (int)strlen(baseUrl), &nLinks);
+									// check for errors indicated by negative values
+									if (nLinks < 0)
+										nLinks = 0;
+									printf("done in %.0f ms with %u links\n", (double)(clock() - t), nLinks);
+									cout << "\n--------------------------------------------------" << endl;
+									cout << header << endl;
+									htmlOut.close();
+									remove("test.html");
+									delete parser;		// this internally deletes linkBuffer
+									delete fileBuf;
+								}
 								}
 							}
 						}
 						else 
 						{
-							//duplicate IP
-							cout << " failed";
+							cout << "Unable to open file";
 						}
-						
-					}
-					else
-					{
-						//duplicate so exit
-						cout << "failed";
 					}
 				}
-				host = port = path = query = "";
-				pch = strtok(NULL, "\n");
+			}
+		}
+	}
+						}
+					}
+				}
+			}
+		}
+		else 
+		{
+			//duplicate IP
+			cout << " failed";
+		}
+						
+	}
+	else
+	{
+		//duplicate so exit
+		cout << "failed";
+	}
+}
+host = port = path = query = "";
+pch = strtok(NULL, "\n");
 			}
 
 		}
